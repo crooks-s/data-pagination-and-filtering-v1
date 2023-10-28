355a2dba7bb6ef7c889d4dec22c843adaa56de00
Sean Crooks <115957278+crooks-s@users.noreply.github.com>
2023-10-28 13:56:05 -0700

    add/complete showPage fxn

--- js/script.js
+++ js/script.js
@@ -17,7 +17,32 @@ For assistance:
 Create the `showPage` function
 This function will create and insert/append the elements needed to display a "page" of nine students
 */
+function showPage(list, page){
+   startIndex = (page * 9) - 9;
+   endIndex = page * 9;
 
+   const studentList = document.querySelector('.student-list');
+   studentList.innerHTML = '';
+
+   for (let i=0; i<list.length; i++){
+      if (i >= startIndex && i < endIndex){
+         let html = `
+         <li class="student-item cf">
+         <div class="student-details">
+           <img class="avatar" src="${list[i].picture.large}" alt="Profile Picture">
+           <h3>${list[i].name.first + ' ' + list[i].name.last}</h3>
+           <span class="email">${list[i].email}</span>
+         </div>
+         <div class="joined-details">
+           <span class="date">Joined ${list[i].registered.date}</span>
+         </div>
+       </li>       
+         `;
+
+         studentList.insertAdjacentHTML('beforeend', html);
+      }
+   }
+}
 
 
 /*
